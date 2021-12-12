7f32f5c43fd086f37934ca55803ac999400bfeae
neolless <neolless@gmail.com>
2021-12-13 00:14:50 +0200

    I

---
+++ 1 ex/Program.cs
@@ -0,0 +1,24 @@
+﻿//Ryzhuk
+// Последовательно задаются вопросы. В результате вся информация
+//выводится в одну строчку
+//- используя склеивание;
+//- используя форматированный вывод;
+//- используя вывод со знаком $;
+
+Console.WriteLine("Введите свое имя : ");
+string name = Console.ReadLine();
+Console.WriteLine ("Введите свою фамилию : ");
+string surname = Console.ReadLine();
+Console.WriteLine ("Введите свой возраст : ");
+string age = Console.ReadLine();
+Console.WriteLine ("Введите свой рост : ");
+string height = Console.ReadLine();
+Console.WriteLine ("Введите свой вес : ");
+string weight = Console.ReadLine();
+
+
+Console.WriteLine (name + surname + age + height + weight );
+string format = String.Format("{0} {1} {2} {3} {4}",name, surname, age, height, weight);
+Console.WriteLine (format);
+Console.WriteLine ($"{name} {surname} {age} {height} {weight}");
+
