91c39f0a7b897885c84c6f4aa8518df7fab616f5
neolless <neolless@gmail.com>
2021-12-13 16:34:51 +0200

    add lesson

--- 1 ex/2 ex/Program.cs
+++ 1 ex/2 ex/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Ryzhuk
+// Ввести вес и рост человека. Рассчитать и вывести индекc
+//массы тела. Где m - масса в кг; h - рост в метрах;
+Console.WriteLine("Введите свой вес");
+string weight = Console.ReadLine();
+double m;
+m = Convert.ToDouble(weight);
+Console.WriteLine("Введите свой рост");
+string height = Console.ReadLine();
+double h;
+h = Convert.ToDouble(height);
+double i = m/(h*h);
+Console.WriteLine($"Индекс массы тела : {i:F3} ! ");
