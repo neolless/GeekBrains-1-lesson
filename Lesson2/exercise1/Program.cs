c56c309002db4f2d6488df3ca2747465df95d94a
neolless <neolless@gmail.com>
2021-12-14 03:23:32 +0200

    add

---
+++ Lesson2/exercise1/Program.cs
@@ -0,0 +1,23 @@
+﻿//ryzhuk
+//Написать программу, возвращающую минимальное
+//из трёх чисел
+Console.Write("a= ");
+int a = int.Parse(Console.ReadLine());
+Console.Write ("b= ");
+int b = int.Parse(Console.ReadLine());
+Console.Write ("c= ");
+int c = int.Parse(Console.ReadLine());
+int min = 0;
+if (a < b && a < c)
+{
+    min = a;
+}
+else if (b < a && b < c)
+{
+    min = b;
+}
+else {
+    min = c;
+}
+Console.WriteLine($"Минимальное число : {min} !");
+
---
+++ Lesson2/exercise2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Ryzhuk
+//Написать программу подсчета
+//количества цифр числа
+
+Console.Write ("Введите число : ");
+int num = int.Parse(Console.ReadLine());
+int i = 0;
+for (i=0; num>0; i++)
+{
+    num/=10;
+}
+Console.WriteLine($"Количество цифр введенного вами числа равна : {i}");
