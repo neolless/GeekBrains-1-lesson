8afd01f955cdf0884638f8a190a26cd4ad7bfebb
neolless <neolless@gmail.com>
2021-12-16 03:47:00 +0200

    adD

--- Lesson2/exercise3/Program.cs
+++ Lesson2/exercise3/Program.cs
@@ -1,1 +1,19 @@
 ﻿
+
+   int b = 0;
+   while (true)
+   {
+  int a = Convert.ToInt16(Console.ReadLine());             
+   if (a != 0)
+   {
+   if (a % 2 == 0)
+  { }
+   else
+   b += a;
+   }
+   else
+  {
+  break;
+   }
+}
+Console.WriteLine(b);
