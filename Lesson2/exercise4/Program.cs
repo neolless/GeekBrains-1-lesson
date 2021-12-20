42ad2a7e8afc84a9002632f99439bcf091bd9e1f
neolless <neolless@gmail.com>
2021-12-21 01:39:40 +0200

    add ex 4

--- Lesson2/exercise4/Program.cs
+++ Lesson2/exercise4/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿//Ryzhuk
+//реализовать метод проверки логина и пароля. На вход метода подается логин и пароль
+//На выходе истина, если пользователь прошел авторизацию,
+//и ложь если не прошел. С помощью цикла do while
+//ограничить ввод пароля 3-мя попытками
+
+string log = "root";
+string password = "GeekBrains";
+int i = 0;
+do {
+
+Console.WriteLine("Введите логин : ");
+string userLog = Console.ReadLine();
+Console.WriteLine("Введите пароль : ");
+string userPassword = Console.ReadLine();
+if (log == userLog && password == userPassword)
+Console.WriteLine("Вы прошли авторизацию");
+else i++;
+}
+while (i<3);
+Console.WriteLine("Вы  не прошли авторизацию");
