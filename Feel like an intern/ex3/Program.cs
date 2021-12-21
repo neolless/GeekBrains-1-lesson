bd3a9087736bc333962c7c6ca63c68463c509db6
neolless <neolless@gmail.com>
2021-12-21 22:42:07 +0200

    add ex 3

---
+++ Feel like an intern/ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Ryzhuk
+//По заданному номеру дня недели вывести его название
+
+using System;
+class Name
+{
+    enum Days{
+     None=0,
+              Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
+     };
+static void Main()
+{
+Console.Write ("Введите число :");
+int userN = int.Parse(Console.ReadLine());
+Console.WriteLine((Days)userN);
+    }
+}
