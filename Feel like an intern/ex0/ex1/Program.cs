47c08d537ff952b71e86f6635ec657ff972ba46e
neolless <neolless@gmail.com>
2021-12-21 17:48:33 +0200

    add ex 1

---
+++ Feel like an intern/ex0/ex1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Ryzhuk
+// вывести квадрат числа
+
+Console.WriteLine ("Введите число : "); //запрос числа у пользователя
+int userNumb = int.Parse(Console.ReadLine()); //конвертация строки в число
+
+Console.WriteLine ($"Квадрат числа :  {userNumb*userNumb}"); //выводим квадрат числа
