3fe112a2fc7e042a727e8e867d03b1d5ad4a1540
neolless <neolless@gmail.com>
2021-12-21 21:13:00 +0200

    add ex2

---
+++ Feel like an intern/ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Ryzhuk
+//Даны два числа. Показать большее и меньшее число
+Console.WriteLine("Введите первое число : "); 
+int userNumb1 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите второе число : "); 
+int userNumb2 = int.Parse(Console.ReadLine()); 
+
+if (userNumb1>userNumb2)
+Console.WriteLine ($"Число {userNumb1} больше {userNumb2}");
+else
+Console.WriteLine ($"Число {userNumb2} больше {userNumb1}");
