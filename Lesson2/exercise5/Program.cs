67e9b3a8d2c96eaf4807def000e85182e27c2ac6
neolless <neolless@gmail.com>
2021-12-21 23:28:08 +0200

    add ex 5

---
+++ Lesson2/exercise5/Program.cs
@@ -0,0 +1,25 @@
+﻿//Ryzhuk
+// Написать программу, которая запрашивает массу и рост человека, 
+// вычисляет его индекс массы и сообщает, нужно ли человеку похудеть, 
+// набрать вес или всё в норме.
+
+Console.WriteLine("Введите свой вес в килограммах : ");
+int userWeight = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите свой рост в метрах : ");
+double userHeight = Convert.ToDouble(Console.ReadLine());
+double imt= Convert.ToDouble(userWeight/(userHeight*userHeight));
+int max = 25;
+int min = 18;
+
+if (imt > max)
+{
+Console.WriteLine("Избыточная масса тела ");
+}
+else if (imt < min)
+{
+Console.WriteLine("Недостаточная масса тела ");
+}
+else 
+Console.WriteLine("Ты в норме");
+
+
